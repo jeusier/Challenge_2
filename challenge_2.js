e3302e9c71e5d95790e1022adb4e44ab495828d4
jahsan87 <jahsan87@gmail.com>
2014-02-26 11:09:41 -1000

    finished test 1

--- challenge_2.js
+++ challenge_2.js
@@ -1,6 +1,25 @@
 exports.sumOfEvenFibonacciNumbers = function(n){
   var sum = 0;
   // do your work here
-  
+  var baseArray = [0, 1, 1];
+  //var baseLength = baseArray.length;
+  //console.log("sum before: "+sum);
+
+  for (var i = 0; i < n; i++) {
+    var sumOfTwoNumbers = baseArray[baseArray.length - 1] + baseArray[baseArray.length - 2];
+    //console.log("i "+i);
+    //console.log("sum: "+sumOfTwoNumbers);
+    if (i == sumOfTwoNumbers) {
+        //console.log("i: "+i);
+        baseArray.push(i);
+        if (i%2 == 0){
+
+        sum += i;
+    }
+        //console.log("sum after: "+sum);
+    }
+
+  }
+  console.log(baseArray);
   return sum;
 };
